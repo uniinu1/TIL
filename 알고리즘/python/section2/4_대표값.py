ad09d464164d63981585948c63e56f2eb78baff8
Sum <37801073+uniinu1@users.noreply.github.com>
2023-05-04 19:44:43 +0900

    Update 4_대표값.py

--- 알고리즘/python/section2/4_대표값.py
+++ 알고리즘/python/section2/4_대표값.py
@@ -3,14 +3,14 @@
 #N명의 학생 중 평균에 가장 가까운 학생은 몇 번째 학생인지
 
 import sys
-#sys.stdin=open("input.txt", "rt")
+#sys.stdin=open("in1.txt", "rt")
 
 T=int(input())
 a=list(map(int, input().split()))
 b= []
 c= []
 mean = round(sum(a) / len(a))
-print(a)
+
 for t in range(T):
     diff = abs(round(a[t] - mean,1))
     b.append(diff)
@@ -23,10 +23,9 @@ for z in range(len(b)):
 for z in range(len(b)):
     if compare == b[z]:
         c.append(a[z])
-print(c)
+
 maxInt = max(c)
 
-print(mean, a.index(maxInt))
+print(mean, a.index(maxInt)+1)
 
-#Answer
-
+# Answer
