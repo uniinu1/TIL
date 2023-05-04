05543e103101d1f08a29537632c1723236e1c44d
Sum <37801073+uniinu1@users.noreply.github.com>
2023-05-04 19:56:45 +0900

    Update 6_자릿수의합.py

--- 알고리즘/python/section2/6_자릿수의합.py
+++ 알고리즘/python/section2/6_자릿수의합.py
@@ -4,20 +4,21 @@
 # 자연수 자릿수  합 구하는 함수를  def digit_sum(x)로 
 
 import sys
-sys.stdin=open("input.txt", "rt")
+#sys.stdin=open("in1.txt", "rt")
 
 T = int(input())
 a = list(map(int, input().split()))
 list = []
-def sumFunc(v):
+def digit_sum(v):
     answer = 0
     for n in str(v):
         answer+=int(n)
     return answer
 
 for t in range(T):
-    sumFunc(str(a[t]))
-    list.append(sumFunc(str(a[t])))
+    digit_sum(str(a[t]))
+    list.append(digit_sum(str(a[t])))
 
-list = sorted(list)
-print(list)          
+
+print(a[list.index(max(list))])          
+
