a7ecae27b9b24d0ea2621dbf3d20c24d1af57104
Sum <37801073+uniinu1@users.noreply.github.com>
2023-05-10 01:56:44 +0900

    Update 6_자릿수의합.py

--- 알고리즘/python/section2/6_자릿수의합.py
+++ 알고리즘/python/section2/6_자릿수의합.py
@@ -22,3 +22,49 @@ for t in range(T):
 
 print(a[list.index(max(list))])          
 
+#answer1 
+import sys
+#sys.stdin=open("in1.txt", "r")
+
+n = int(input())
+a = list(map(int, input().split()))
+
+def digit_sum(x):
+    sum = 0
+    while x > 0:
+        sum+=x%10
+        x=x//10
+
+    return sum
+
+max = -2147000000
+for x in a:
+    tot = digit_sum(x)
+    if tot > max:
+        max = tot
+        res = x
+
+print(res)
+
+
+#answer2
+import sys
+#sys.stdin=open("in1.txt", "r")
+
+n = int(input())
+a = list(map(int, input().split()))
+
+def digit_sum(x):
+    sum = 0
+    for i in str(x):
+        sum+=int(i)
+    return sum
+
+max = -2147000000
+for x in a:
+    tot = digit_sum(x)
+    if tot > max:
+        max = tot
+        res = x
+
+print(res)
