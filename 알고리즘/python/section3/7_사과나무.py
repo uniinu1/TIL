5b0289cd58253031f29fa92bb0e5c26cf8e033df
Sum <37801073+uniinu1@users.noreply.github.com>
2023-09-24 07:16:41 +0900

    Update 7_사과나무.py

--- 알고리즘/python/section3/7_사과나무.py
+++ 알고리즘/python/section3/7_사과나무.py
@@ -35,10 +35,11 @@ s = 1
 e = n // 2
 
 for i in range(n):
-    if n < e:
+    if i < e:
         for z in range(s):
             if(n == 1):
                 result += a[e]
+                # 간격을 2만큼 늘리기 위함
                 s += 2
             elif(n != 1):
                 a
