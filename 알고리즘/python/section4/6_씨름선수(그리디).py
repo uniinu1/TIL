e53b50fdcae084404d566d98ea3feae5e4ac4932
Sum <37801073+uniinu1@users.noreply.github.com>
2023-09-27 20:28:48 +0900

    Update 6_씨름선수(그리디).py

--- 알고리즘/python/section4/6_씨름선수(그리디).py
+++ 알고리즘/python/section4/6_씨름선수(그리디).py
@@ -6,6 +6,7 @@
 
 #키와 몸무게를 같이 넣고 이중 for문 돌면 시간복잡도 높아지므로, 최대값 구하는 과정처럼 몸무게를 확인하자
 
+'''
 import sys
 sys.stdin=open("input.txt", "r")
 
@@ -25,4 +26,26 @@ for x, y in body:
     if y>largest:
         largest=y
         cnt+=1
+print(cnt)
+'''
+
+# my answer
+import sys
+sys.stdin=open("D:\강의\파이썬 알고리즘 문제풀이(코딩테스트 대비)\code\section4\input_6.txt", "r")
+n=int(input())
+people=[]
+for i in range(n):
+    a, b=map(int, input().split())
+    people.append((a, b))
+
+people.sort(reverse=True)
+cnt = 1
+bWeight = people[0][1]
+people.pop(0)
+
+for a in range(n - 1):
+    if bWeight < people[a][1]:
+        cnt += 1
+        bWeight = people[a][1]
+    
 print(cnt)
