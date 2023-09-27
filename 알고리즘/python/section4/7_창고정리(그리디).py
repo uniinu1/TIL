b5bd5b2a6a3470677aaf3d51743ab8b83b053f6e
Sum <37801073+uniinu1@users.noreply.github.com>
2023-09-27 23:22:32 +0900

    Update 7_창고정리(그리디).py

--- 알고리즘/python/section4/7_창고정리(그리디).py
+++ 알고리즘/python/section4/7_창고정리(그리디).py
@@ -1,8 +1,13 @@
+import math
+import time
 #창고 정리
 # 처음 든 생각 : 배열에 넣고 max, min 함수 이용해서 하나씩 하나씩 돌면 풀 수는 있는데, 조정 횟수에 따라 시간이 많이 걸릴 수 있겠다
+start = time.time()
+math.factorial(100000)
+end = time.time()
 '''
 import sys
-sys.stdin=open("input.txt", "r")
+sys.stdin=open("D:\강의\파이썬 알고리즘 문제풀이(코딩테스트 대비)\code\section4\input_6.txt", "r")
 L=int(input())
 a=list(map(int, input().split()))
 m=int(input())
@@ -13,13 +18,13 @@ for _ in range(m):
     a.sort()
 
 print(a[L-1]-a[0])
+
 '''
-#그냥 내 생각처럼 풀었네,,? max. min만 안 썼을 뿐
 #max, min과 sort방식 시간 비교해보기
 
 #my answer
 import sys
-#sys.stdin = open("D:\강의\파이썬 알고리즘 문제풀이(코딩테스트 대비)\code\section4\input_6.txt", "r")
+sys.stdin = open("D:\강의\파이썬 알고리즘 문제풀이(코딩테스트 대비)\code\section4\input_6.txt", "r")
 n = int(input())
 height = list(map(int, input().split()))
 qn = int(input())
@@ -34,3 +39,5 @@ for a in range(qn):
     height[minIndex] = height[minIndex] + 1
 
 print(max(height) - min(height))
+
+print(f"{end - start:.5f} sec")
