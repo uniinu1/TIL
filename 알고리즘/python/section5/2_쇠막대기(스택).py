dc375dd37ed76d33b636131f91f3b3565a94c6c7
Sum <37801073+uniinu1@users.noreply.github.com>
2023-09-28 12:50:03 +0900

    Update 2_쇠막대기(스택).py

--- 알고리즘/python/section5/2_쇠막대기(스택).py
+++ 알고리즘/python/section5/2_쇠막대기(스택).py
@@ -2,11 +2,15 @@
 # 여는 괄호는 무조건 append 하기 
 # 닫는 괄호를 만나면 앞의 데이터를 확인하고 앞의 데이터가 여는 괄호면 여는 괄호 pop
 # append된 여는 괄호 길이가 막대기 길이임
-# 이거 하면 좋을듯 다시 보면서 이해해보기
+
+'''
+예시
+((()
+
 
 
 import sys
-sys.stdin=open("input.txt", "r")
+sys.stdin=open("D:\강의\파이썬 알고리즘 문제풀이(코딩테스트 대비)\code\section5\input.txt", "rt")
 s=input()
 
 stack=[]
@@ -22,4 +26,21 @@ for i in range(len(s)):
         else:
             cnt+=1
 print(cnt)
+'''
+# my answer
+import sys
+sys.stdin=open("D:\강의\파이썬 알고리즘 문제풀이(코딩테스트 대비)\code\section5\input.txt", "rt")
+inputList = input()
 
+cnt = 0
+stack = []
+for i in range(len(inputList)):
+    if inputList[i] == '(':
+        stack.append(inputList[i])
+    else:
+        stack.pop()
+        if inputList[i-1] == '(':
+            cnt += len(stack)
+        else:
+            cnt += 1
+print(cnt)
